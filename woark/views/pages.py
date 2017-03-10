ab5d2ff898395e0b1f73db1ff362ef14216010ec
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-03-10 22:16:33 +0530

    Home page view added

--- woark/views/pages.py
+++ woark/views/pages.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from woark.models import Site, SiteContact, SiteSocialProfile, Menu, MenuItem
 
 def home(request):
-	return HttpResponse("Welcome To Woark, A Mini Content Management System for managing small websites")
+	template_name = 'woark/pages/home.html'
+	context = {
+	'site': Site.objects.get(id='1'),
+	'site_contact': SiteContact.objects.all(),
+	'site_social_profile': SiteSocialProfile.objects.all(),
+	'header_menu': MenuItem.objects.filter(menu__name = 'Header Menu')
+	}
+	return render(request, template_name, context)
+	
