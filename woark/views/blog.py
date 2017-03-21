39a8e3f313b1f0c1a6d63c7bd8e477c18819641e
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-03-21 05:52:51 +0530

    Comment Form properly working, added working view and template, refine it

--- woark/views/blog.py
+++ woark/views/blog.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from woark.models import Article, ArticleContent
+from django.contrib.auth.models import User
+from woark.models import Article, ArticleContent, Comment
+from woark.forms import ArticleCommentForm
 
 def blog(request):
 	pass
@@ -9,10 +11,34 @@ def blog_article(request):
 
 def get_article(request, article_id):
 	template_name = 'woark/blog/get_article.html'
+	form = ArticleCommentForm()
+	article = Article.objects.get(id=article_id)
+	article_comment = article.comment.all()
 	context = {
-		'article': Article.objects.get(id=article_id),
+		'article': article,
 		'article_content': ArticleContent.objects.get(article = article_id),
-		
+		'c_form':form,
+		'article_comment':article_comment,
 	}
+	formerror = ""
+	if request.method == 'POST':
+		if request.user.is_authenticated:
+			if request.user.is_active:
+				form = ArticleCommentForm(request.POST)
+				if form.is_valid():
+					comment = form.save(commit=False)
+					comment.article = article
+					comment.user = request.user
+					comment.save()
+				else:
+					context['c_form']=form
+			else:
+				formerror = "Sorry Your Account Is Not Active"
+		else:
+			formerror = "Sorry Register First To Comment!"
+
+		context['formerror'] = formerror
 
 	return render(request, template_name, context)
+
+
