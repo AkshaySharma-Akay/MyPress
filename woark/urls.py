0e7d2056d94212fc6b5864d267858c93b4e1638c
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-03-25 07:25:48 +0530

    Added the urls for the blog packs, and blog views

--- woark/urls.py
+++ woark/urls.py
@@ -4,5 +4,12 @@ from . import views
 #urlpatterns 
 urlpatterns = [
 	url(r'^$',views.home, name='home'),
-	url(r'^blog/(?P<article_id>[0-9]+)/$',views.get_article, name='get_article'),
+	url(r'^blog/(?P<blog_id>[0-9]+)/$', views.get_blog, name='get_blog'),
+	url(r'^blog/(?P<blog_id>[0-9]+)/pack/(?P<pack_count>[0-9]+)/$', views.get_blog_pack, name='get_blog_pack'),
+	url(r'^blog/(?P<blog_id>[0-9]+)/(?P<article_id>[0-9]+)/$',views.get_article, name='get_article'),
+
+	#pages link 
+	#url(r'^aboutus/$',views.aboutus, name='aboutus'),
+	#url(r'^contactus/$', views.contactus, name='contactus'),
+	
 ]
