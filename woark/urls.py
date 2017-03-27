2c6cc198a57793a4ec53f0b89095b834d3fba419
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-03-27 12:37:24 +0530

    Stable version of the urls to be improved

--- woark/urls.py
+++ woark/urls.py
@@ -3,13 +3,16 @@ from . import views
 
 #urlpatterns 
 urlpatterns = [
-	url(r'^$',views.home, name='home'),
+	url(r'^$', views.home, name='home'),
+	url(r'^blog/$', views.blog , name='blog'),
+
+	#blogs
 	url(r'^blog/(?P<blog_id>[0-9]+)/$', views.get_blog, name='get_blog'),
 	url(r'^blog/(?P<blog_id>[0-9]+)/pack/(?P<pack_count>[0-9]+)/$', views.get_blog_pack, name='get_blog_pack'),
+
+	#articles
 	url(r'^blog/(?P<blog_id>[0-9]+)/(?P<article_id>[0-9]+)/$',views.get_article, name='get_article'),
 
-	#pages link 
-	#url(r'^aboutus/$',views.aboutus, name='aboutus'),
-	#url(r'^contactus/$', views.contactus, name='contactus'),
-	
+	#tags
+	url(r'^blog/(?P<blog_id>[0-9]+)/tags/$', views.get_tags, name = 'get_tags'),
 ]
