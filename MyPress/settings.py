e5c351811483bbb5be26ca29d22e9cd0eed9d31c
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-04-02 20:00:46 +0530

    Updated the settings.py and urls.py to add evo app in the project

--- MyPress/settings.py
+++ MyPress/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'evo.apps.EvoConfig',
     'woark.apps.WoarkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- MyPress/urls.py
+++ MyPress/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('woark.urls')),
+    url(r'', include('evo.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
