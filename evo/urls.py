c76764c1173091bcbd51716da73044ef31c6ce60
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-04-03 06:26:27 +0530

    Added the url to the index page and the login page

--- evo/urls.py
+++ evo/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-	
+	url(r'^evo/$',views.evo_index, name='evo_index'),
+	url(r'^evo/login/$',views.signin, name='signin'),
 ]
