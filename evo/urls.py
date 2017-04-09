fa81d81f8975df0e5c22d1e83df3d99f2cabffc2
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-04-09 09:05:21 +0530

    Added the url for the student signup

--- evo/urls.py
+++ evo/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^evo/$',views.evo_index, name='evo_index'),
-	url(r'^evo/login/$',views.signin, name='signin'),
+	url(r'^login/$',views.signin, name='signin'),
+	url(r'^signup/$',views.signup, name='signup'),
+	url(r'^tutor/dashboard/$', views.tutor_dashboard, name='tutor_dashboard'),
+	url(r'^student/dashboard/$', views.student_dashboard, name='student_dashboard'),
+	url(r'^deo/dashboard/$', views.deo_dashboard, name='deo_dashboard'),
 ]
