7203e99e7b6124fce6d0ff6eda84806a824a82fc
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-04-12 11:10:32 +0530

    Added the basic version of the views for the tutors, to be updated once done with the admission views

---
+++ evo/views/tutor.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from checkviews import *
+
+
+@login_required(login_url = '/evo/login/')
+def tutor_dashboard(request):
+	if is_tutor(request):
+		return HttpResponse("Welcome Tutor")
+	else:
+		return HttpResponse("Sorry You Can't Access This Page.")
