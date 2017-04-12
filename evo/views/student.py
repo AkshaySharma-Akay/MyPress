7acfa9dc1dc78ba506b1f51440f7a05ff6069bca
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-04-12 11:09:44 +0530

    Important Commit, Added the student_Welcome screen views and added the student_dashboard views, most important view,as some of the other views depends on this view

---
+++ evo/views/student.py
@@ -0,0 +1,50 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse 
+from evo.models import EvoUser, StatusStudent
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from checkviews import *
+
+
+#redirect student a/c to his status
+@login_required(login_url = '/evo/login/')
+def student_dashboard(request):
+	if is_student(request):		
+		try:
+			status  = StatusStudent.objects.get(student = request.user)
+			if status.admission == False:
+				return redirect('student_welcome')
+			elif status.profile == False:
+				return redirect('student_profile')
+			else:
+				return redirect('student')
+		except StatusStudent.DoesNotExist:
+			return HttpResponse("Sorry Something Went Wrong Contact Administration")
+
+		return HttpResponse('Welcome USer')
+	else:
+		return HttpResponse("Sorry You Can't Access This Page")
+
+
+@login_required(login_url = '/evo/login/')
+def student_welcome(request):
+	if is_student(request):
+		template_name ='evo/admission/welcome.html'
+		context = {
+			'user':request.user,
+		}
+		return render(request, template_name, context)
+	else:
+		return HttpResponse("Sorry You Can't Access This Page")
+
+
+#Same For All Views
+"""
+@login_required(login_url = '/evo/login/')
+def student_welcome(request):
+	if is_student(request):
+		return HttpResponse('Welcome USer')
+	else:
+		return HttpResponse("Sorry You Can't Access This Page")
+"""
