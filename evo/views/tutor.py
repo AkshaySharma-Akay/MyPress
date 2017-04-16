07b26f77d4851c9448880b9da5572d569e0a5ba9
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-04-16 08:19:07 +0530

    Added the is_tutor() function to the view and removed the dependency on the checkviews in this module too..

--- evo/views/tutor.py
+++ evo/views/tutor.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from checkviews import *
 
+@login_required(login_url = '/evo/login/')
+def is_tutor(request):
+	user = request.user
+	if user.evouser.account_type == 't':
+		return True
+	else:
+		return False
 
 @login_required(login_url = '/evo/login/')
 def tutor_dashboard(request):
